64df85e308ade5ec488d064aeb99dcad63575db1
jaemin <wjdwoals9292@gmail.com>
2023-10-20 17:16:54 +0900

    [FEATURE][COMMON] Test Code 구현

---
+++ src/test/java/com/ultimatestorytelling/backend/member/command/application/service/MemberServiceTest.java
@@ -0,0 +1,32 @@
+package com.ultimatestorytelling.backend.member.command.application.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void checkDuplicateMemberNickname() {
+    }
+
+    @Test
+    void checkDuplicateMemberEmail() {
+    }
+
+    @Test
+    void deleteMember() {
+    }
+
+    @Test
+    void changeMemberPwd() {
+    }
+
+    @Test
+    void updateMemberInfo() {
+    }
+}
