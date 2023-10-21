8542ad810c49849a70818f266c0d42e1a41c5028
jaemin <wjdwoals9292@gmail.com>
2023-10-21 09:39:00 +0900

    [FEATURE][COMMON] Test Code 구현

---
+++ src/test/java/com/ultimatestorytelling/backend/login/command/application/service/LoginServiceTest.java
@@ -0,0 +1,16 @@
+package com.ultimatestorytelling.backend.login.command.application.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginServiceTest {
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void renewAccessToken() {
+    }
+}
